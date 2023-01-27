090cdb40c4c714c855566a75c5fb038271b1f24a
Victor Varbanov <9961253+TresV@users.noreply.github.com>
2023-01-28 00:34:43 +0200

    added exercise 4

---
+++ 3.0-JS-Fundamentals-Arrays-Lab/04-revers-in-place.js
@@ -0,0 +1,13 @@
+function reverseInPlace(input) {
+
+    for(let i = 0; i < input.length / 2; i++) {
+        let result = input[i];
+
+        input[i] = input[input.length - 1 - i];
+        input[input.length - 1 - i] = result;
+
+    }
+    console.log(input.join(' '));
+}
+
+reverseInPlace(['a', 'b', 'c', 'd', 'e']);
