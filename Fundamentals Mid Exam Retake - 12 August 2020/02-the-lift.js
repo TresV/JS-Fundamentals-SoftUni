4dcbd45ae8ecdfb925f792376cdd395379d1feed
Victor Varbanov <9961253+TresV@users.noreply.github.com>
2023-02-24 20:49:24 +0200

    update

--- Fundamentals Mid Exam Retake - 12 August 2020/02-the-lift.js
+++ Fundamentals Mid Exam Retake - 12 August 2020/02-the-lift.js
@@ -1,50 +1,4 @@
 function theLift(input) {
-    let arr = input.slice();
-    let peopleInQueue = Number(arr[0]);
-    let wagons = arr[1].split(" ").map(Number);
-    let peopleToAddFromQueue = 0;
-
-    for (let i = 0; i < wagons.length; i++) {
-        let wagonCurrentCapacity = wagons[i];
-
-        if (wagonCurrentCapacity < 4) {
-            let freeSeats = 4 - wagonCurrentCapacity;
-
-            if (freeSeats > peopleInQueue) {
-                peopleToAddFromQueue = peopleInQueue;
-            } else {
-                peopleToAddFromQueue =
-                    peopleInQueue - (peopleInQueue - freeSeats);
-            }
-
-            wagons[i] = wagonCurrentCapacity + peopleToAddFromQueue;
-            peopleInQueue -= peopleToAddFromQueue;
-        }
-
-        if (peopleInQueue === 0) {
-            break;
-        }
-    }
-
-    let peopleInLift = wagons.reduce((a, b) => a + b, 0);
-    let liftCapacity = wagons.length * 4;
-
-    if (peopleInLift < liftCapacity && peopleInQueue === 0) {
-        console.log(`The lift has empty spots!\n${wagons.join(" ")}`);
-    } else if (peopleInLift === liftCapacity && peopleInQueue > 0) {
-        console.log(
-            `There isn't enough space! ${peopleInQueue} people in a queue!\n${wagons.join(
-                " "
-            )}`
-        );
-    } else if (peopleInLift === liftCapacity && peopleInQueue === 0) {
-        console.log(lift.join(" "));
-    }
-}
-theLift(["15", "0 0 0 0 0"]);
-// theLift(["20", "0 2 0"]);
-
-function theLift(input) {
     let queue = Number(input[0]);
     let lift = input[1].split(" ").map(Number);
     let peopleToAddFromQueue = 0;
@@ -85,3 +39,7 @@ function theLift(input) {
         console.log(lift.join(" "));
     }
 }
+theLift(["15", "0 0 0 0 0"]);
+// theLift(["20", "0 2 0"]);
+
+
---
+++ Fundamentals Mid Exam Retake - 12 August 2020/03-memory-game.js
@@ -0,0 +1,5 @@
+function memoryGame(input) {
+    let sequence = input.shift().split(' ');
+    
+}
+memoryGame(["1 1 2 2 3 3 4 4 5 5", "1 0", "-1 0", "1 0", "1 0", "1 0", "end"]);
