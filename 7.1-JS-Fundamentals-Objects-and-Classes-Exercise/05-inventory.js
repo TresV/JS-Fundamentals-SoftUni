1e488e8fd795f2abddded1b73d94c34b37b84b6a
Victor Varbanov <9961253+TresV@users.noreply.github.com>
2023-03-03 18:38:50 +0200

    Create 05-inventory.js

---
+++ 7.1-JS-Fundamentals-Objects-and-Classes-Exercise/05-inventory.js
@@ -0,0 +1,26 @@
+function heroList(list){
+    let heroes = [];
+
+    list.forEach(line => {
+        let [name, level, items] = line.split(' / ');
+        let currentHero = {
+            name,
+            level:Number(level),
+            items
+        };
+        heroes.push(currentHero)
+    });
+    heroes.sort((a,b) => a.level - b.level)
+
+    heroes.forEach((hero) =>{
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items}`);
+    })
+}
+heroList([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+)
